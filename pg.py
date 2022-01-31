62ebcec6aa2a8ef5f159f3b3b88b9758056df0b2
Pranavkool <98758049+Pranavkool@users.noreply.github.com>
2022-01-31 21:59:28 +0530

    Update pg.py

--- pg.py
+++ pg.py
@@ -2,11 +2,11 @@ import random
 
 input("This program generates easy-to-remember passwords by adding hints typed in by the user")
 bruh = input("Enter a hint to be added to your password. If there are multiple, seperate them with commas")
-size = int(input("Enter password length"))
+size = int(input("Enter required password length"))
 
 bruh2 = list(bruh)
 
-ultrabruh = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0',"!","@","#","$","%","^","&",'*','(',')','_',"+",'-','=','{','}','|',':','<',"."]
+ultrabruh = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G',H','J','K','L','Z','X,'C','V','B','N','M','1','2','3','4','5','6','7','8','9','0',"!","@","#","$","%","^","&",'*','(',')','_',"+",'-','=','{','}','|',':','<',"."]
 
 lmfao = random.sample(ultrabruh, size)
 str1 = ''
