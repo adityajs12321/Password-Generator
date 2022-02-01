986b6ad63e8c9ab0da7c96fcadeffc5acab282c8
Pranavkool <98758049+Pranavkool@users.noreply.github.com>
2022-02-01 13:52:33 +0530

    Update pg.py

--- pg.py
+++ pg.py
@@ -1,7 +1,7 @@
 import random
 
 input("This program generates easy-to-remember passwords by adding hints typed in by the user")
-bruh = input("Enter a hint to be added to your password. If there are multiple, seperate them with commas")
+bruh = input("Enter a hint to be added to your password. If there are multiple, separate them with commas")
 size = int(input("Enter required password length"))
 
 bruh2 = list(bruh)
