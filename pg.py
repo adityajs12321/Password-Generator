56e67b2ad81d771da4f8a8da03e843245c5f5ebb
EmperoarV <77228612+AdityaJS12321@users.noreply.github.com>
2022-02-04 09:44:04 +0530

    Almost done

--- pg.py
+++ pg.py
@@ -2,21 +2,28 @@ import random
 
 input("This program generates easy-to-remember passwords by adding hints typed in by the user")
 bruh = input("Enter a hint to be added to your password. If there are multiple, separate them with commas")
-size = int(input("Enter required password length"))
 
+import nltk
 from nltk.corpus import wordnet
 
 bruh2 = list(bruh)
 
-ultrabruh = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G',H','J','K','L','Z','X,'C','V','B','N','M','1','2','3','4','5','6','7','8','9','0',"!","@","#","$","%","^","&",'*','(',')','_',"+",'-','=','{','}','|',':','<',"."]
+ultrabruh = ['1','2','3','4','5','6','7','8','9','0']
 
-lmfao = random.sample(ultrabruh, size)
+lmfao = random.sample(ultrabruh, 4)
+hints = []
 
 synonyms = wordnet.synsets("hint")
-print(synonyms)
+for ss in synonyms :
+  if ss.name() not in hints :
+     hints.append(ss.name())
+  
+hintsFinal = random.sample(hints, 2)
 
-str1 = ''.join(lmfao)
+final = hintsFinal+lmfao
 
-print("Password: ", str1)
+str1 = ''.join(final)
+
+print("Password: ", final)
 #JS sux
 #prawnie sux
