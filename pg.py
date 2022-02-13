d5756ecc8d05976be0e0683a5fbf3d0ff56c0f11
EmperoarV <77228612+AdityaJS12321@users.noreply.github.com>
2022-02-13 15:41:21 +0530

    Major Update

--- pg.py
+++ pg.py
@@ -1,29 +1,55 @@
 import random
-
-
-input("This program generates easy-to-remember passwords by adding hints typed in by the user")
-bruh = input("Enter a hint to be added to your password")
-
+import tkinter
 from nltk.corpus import wordnet
 
-bruh2 = list(bruh)
+mac = tkinter.Tk()
+mac.title('Password Generator')
+mac.geometry('450x300')
 
-ultrabruh = ['1','2','3','4','5','6','7','8','9','0']
+def DeleteEntry() :
+  b.destroy()
+  c.destroy()
 
-lmfao = random.sample(ultrabruh, 4)
-hints = []
+def true():
+  global c
+  global b
+  c = tkinter.Button(mac, text='Delete Entry', command = DeleteEntry)
+  c.place(x=90, y=60)
+  global b
+  print("Working")
+  inp = inputtxt.get(1.0, 'end-1c')
+  tkinter.Label(mac, text=inp).place(x=10, y=10)
+  print(inp)
 
-synonyms = wordnet.synsets(bruh)
-for ss in synonyms :
-  if ss.name()[:len(ss.name())-4] not in hints :
-     hints.append(ss.name()[:len(ss.name())-4])
+  numbers = ['1','2','3','4','5','6','7','8','9','0']
+
+  randomized = random.sample(numbers, 4)
+  global hints
+  hints = []
+
+  synonyms = wordnet.synsets(inp)
+  for ss in synonyms :
+    if ss.name()[:len(ss.name())-4] not in hints :
+       hints.append(ss.name()[:len(ss.name())-4])
+  if len(hints) > 1:
+    hintsFinal = random.sample(hints, 2)
+  else :
+    hintsFinal = hints
+
+  final = hintsFinal+randomized
+
+  str1 = ''.join(final)
+  hmm = 'Password: ' + str1
+  b = tkinter.Label(mac, text=hmm)
+  b.place(relx=0.5, rely=0.75, anchor='center')
   
-hintsFinal = random.sample(hints, 2)
+tkinter.Label(mac, text='Enter a hint to be added to your password').place(x=10, y=40)
+inputtxt = tkinter.Text(mac, height = 1, width =20)
+inputtxt.place(relx = 0.78, rely = 0.18,anchor = 'center')
+tkinter.Button(mac, text='Enter', command = true).place(x=10,y=60)
 
-final = hintsFinal+lmfao
 
-str1 = ''.join(final)
 
-print("Password: ", str1)
 #JS sux
 #prawnie sux
+tkinter.mainloop()
